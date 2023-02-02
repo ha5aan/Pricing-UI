ed0b9c2fe5b842c27c8809b6992670ef04f5918c
Hasaan <muhammad.hasaan@logicielservice.com>
2023-02-02 12:18:10 +0500

    added alt

--- pages/Components/BottomIcons.tsx
+++ pages/Components/BottomIcons.tsx
@@ -6,13 +6,13 @@ const BottomIcons = () => {
   return (
 <Flex justify={"center"} alignContent={"center"} mt={"2%"} direction={{ base: 'column', md: 'column', lg:"row"}}>
   <Box display={"flex"} mx={"2%"} my={"2%"} justifyContent={"center"} alignContent={"center"}>
-    <Image src={"/shield.PNG"} height={40} width={40}/> <Text  pl={"8%"} width={"70%"} fontWeight={"bold"}> 30 days money back Guarantee</Text>
+    <Image src={"/shield.PNG"} height={40} width={40} alt="shield"/> <Text  pl={"8%"} width={"70%"} fontWeight={"bold"}> 30 days money back Guarantee</Text>
   </Box>
   <Box display={"flex"} mx={"2%"}my={"2%"} justifyContent={"center"} alignContent={"center"}>
-    <Image src={"/strip.PNG"} height={40} width={40}/> <Text  pl={"8%"} width={"70%"} fontWeight={"bold"}> 30 days money back Guarantee</Text>
+    <Image src={"/strip.PNG"} height={40} width={40} alt="Strio"/> <Text  pl={"8%"} width={"70%"} fontWeight={"bold"}> 30 days money back Guarantee</Text>
   </Box>
   <Box display={"flex"} mx={"2%"}my={"2%"} justifyContent={"center"} alignContent={"center"} >
-    <Image src={"/reload.PNG"} height={40} width={40}/> <Text  pl={"8%"} width={"70%"} fontWeight={"bold"}> 30 days money back Guarantee</Text>
+    <Image src={"/reload.PNG"} height={40} width={40} alt="Recurring"/> <Text  pl={"8%"} width={"70%"} fontWeight={"bold"}> 30 days money back Guarantee</Text>
   </Box>
 </Flex>
   )
